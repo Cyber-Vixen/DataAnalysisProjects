3c8d74836720ae8eeb058620d02be32b241dac5d
Cyber-Vixen <160995914+Cyber-Vixen@users.noreply.github.com>
2024-03-19 14:53:04 -0400

    Add files via upload

---
+++ SQLQuery1.1.sql
@@ -0,0 +1,11 @@
+SELECT TOP (1000) [continent]
+      ,[location]
+      ,[date]
+      ,[population]
+      ,[new_vaccinations]
+      ,[rollingpeoplevaccinated]
+  FROM [Portfolio Project].[dbo].[percecntpopulationvaccinated]
+
+
+  select *
+  from percecntpopulationvaccinated
